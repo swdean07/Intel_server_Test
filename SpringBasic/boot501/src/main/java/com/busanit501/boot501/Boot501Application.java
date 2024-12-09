902687f273b673b729c32232a91133b36dce8ed0
swdean07 <swdean07@gmail.com>
2024-12-09 16:37:36 +0900

    241209 스프링 부트 내용 실시

---
+++ SpringBasic/boot501/src/main/java/com/busanit501/boot501/Boot501Application.java
@@ -0,0 +1,13 @@
+package com.busanit501.boot501;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Boot501Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Boot501Application.class, args);
+    }
+
+}
